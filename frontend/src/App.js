ff35e73716ba29f207915b86dc0b167f5ead216b
Restyled.io <commits@restyled.io>
2022-07-26 08:42:41 +0000

    Restyled by prettier

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,33 @@
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import Adopt from './pages/Adopt.jsx';
-import Health from './pages/Health.jsx';
-import Home from './pages/Home.jsx';
-import Info from './pages/Info.jsx';
-import Security from './pages/Security.jsx';
+import Adopt from "./pages/Adopt.jsx";
+import Health from "./pages/Health.jsx";
+import Home from "./pages/Home.jsx";
+import Info from "./pages/Info.jsx";
+import Security from "./pages/Security.jsx";
 
 function App() {
   return (
-      <Router><div className = "App"><Routes>
-      <Route path = "/" element = { <Home /> } />
-          <Route path="/adopt
-                                                " element={<Adopt />} />
-                                  < Route path = "/health" element =
-       { <Health /> } />
-          <Route path="/security
-                       " element={<Security />} />
-       < Route path = "/info" element = { <Info /> } />
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/adopt
+                                                "
+            element={<Adopt />}
+          />
+          <Route path="/health" element={<Health />} />
+          <Route
+            path="/security
+                       "
+            element={<Security />}
+          />
+          <Route path="/info" element={<Info />} />
         </Routes>
       </div>
-    </Router>);
+    </Router>
+  );
 }
 
 export default App;
