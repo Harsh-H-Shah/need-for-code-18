4a3a8f755d1a81333bb02207e064125f207e45e4
harsh <harshmkvviv@gmail.com>
2022-07-26 19:04:56 +0530

    :star: add: adoption form and contact us form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import Adopt from "./pages/Adopt.jsx";
-import Health from "./pages/Health.jsx";
-import Home from "./pages/Home.jsx";
-import Info from "./pages/Info.jsx";
-import Security from "./pages/Security.jsx";
+import Adopt from './pages/Adopt.jsx';
+import Health from './pages/Health.jsx';
+import Home from './pages/Home.jsx';
+import Info from './pages/Info.jsx';
+import Security from './pages/Security.jsx';
+import AdoptApp from './pages/AdoptApp.jsx';
 
 function App() {
   return (
@@ -12,17 +13,10 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route
-            path="/adopt
-                                                "
-            element={<Adopt />}
-          />
+          <Route path="/adopt" element={<Adopt />} />
+          <Route path="/adopt/app" element={<AdoptApp />} />
           <Route path="/health" element={<Health />} />
-          <Route
-            path="/security
-                       "
-            element={<Security />}
-          />
+          <Route path="/security" element={<Security />} />
           <Route path="/info" element={<Info />} />
         </Routes>
       </div>
